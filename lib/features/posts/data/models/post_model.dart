31a65fb4443f9a4a1bb6c374651fcaa4627b1e0d
Aasem <assem_soma541@yahoo.com>
2022-06-20 04:52:46 +0200

    post model class implemented

---
+++ lib/features/posts/data/models/post_model.dart
@@ -0,0 +1,17 @@
+import '../../domain/entities/post.dart';
+
+class PostModel extends Post {
+  PostModel({required super.id, required super.title, required super.body});
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(id: json['id'], title: json['title'], body: json['body']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+}
